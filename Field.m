526d230aac14e968b8b0157e8ff1b81110e6c229
SimPhotonicsFMM <132675953+SimPhotonicsFMM@users.noreply.github.com>
2024-11-18 15:30:40 +0100

    Update Field.m
    Add "Field" parameter to compute Field

--- Field.m
+++ Field.m
@@ -1,11 +1,25 @@
 function varargout = Field(index,geom,lambda,theta,inc,varargin)
 
+
 varin = varargin;
+f = varin(1:2:end);
+P = find(ismember(f,'Field'));
+dim = length(varin);
+if isempty(P) || varin{2*P} == 0
+    varin{dim+1} = 'Field';
+    varin{dim+2} = 1;
+%     error('For Field Calculation, set parameter ''Field'' equal to 1 ' )
+end
+%
+if length(lambda)>1 || length(theta)>1
+    error('Calculation allowed for only one value of lambda and theta')
+end
+%
 s = Spectrum(index,geom,lambda,theta,inc,varin);
 
 [x,y,z] = deal(geom.x,geom.y,geom.z);
 
-if isfield(s.Data,'Nsub')
+if isfield(s.Data,'Nsub') && sum(cell2mat({s.Data.Nsub})) ~= 0
     [E,H] = CalculFieldFD_FMM(s,x,y,z);
 else
     [E,H] = CalculFieldFMM(s,x,y,z);
