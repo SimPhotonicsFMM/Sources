4936968ed966533f7b0bb6400aff6742a11a1330
SimPhotonicsFMM <132675953+SimPhotonicsFMM@users.noreply.github.com>
2026-09-11 15:44:41 +0200

    Update MeshGrating1D.m
    From 2D Grating to 1D Grating without simplifying the obtained mesh

--- MeshGrating1D.m
+++ MeshGrating1D.m
@@ -1,5 +1,9 @@
-function [Mesh,index] = MeshGrating1D(Mesh,TabIndex)
+function [Mesh,index] = MeshGrating1D(Mesh,TabIndex,Flag)
 
+% From 2D Grating to 1D Grating
+
+if nargin == 2, Flag = 0; end
+if Flag ==0
 [Mesh.CoorN(:,2), Mesh.CoorN(:,3)] = deal(Mesh.CoorN(:,3),Mesh.CoorN(:,2));
 [Mesh.CoorA(:,2), Mesh.CoorA(:,3)] = deal(Mesh.CoorA(:,3),Mesh.CoorA(:,2));
 [Mesh.CoorF(:,2), Mesh.CoorF(:,3)] = deal(Mesh.CoorF(:,3),Mesh.CoorF(:,2));
@@ -7,6 +11,9 @@ function [Mesh,index] = MeshGrating1D(Mesh,TabIndex)
 Mesh = MoveMesh(utilMesh2(Mesh),[0 -max(Mesh.CoorN(:,2))/2 0]);
 Mesh.zv = Mesh.yv;
 Mesh.yv = [];
+end
+%
+% Transform 3D mesh into slices
 %
 Mesh0 = Mesh;
 clear Mesh;
@@ -43,7 +50,8 @@ for k = 1:length(Mesh1)
     end
 end
 %%
-%%
+%% 
+% Update number of facets according to subdomain number
 Mesh = Mesh1;
 
 for k = 1:length(Mesh1)
@@ -52,8 +60,14 @@ for k = 1:length(Mesh1)
     for km = 1:length(NumSD)
         Pe1 = Mesh1(k).Nsd == NumSD(km);
         Mesh(k).Nsd(Pe1) = km;
-        Pf = Mesh1(k).TabNsdF{1,km}~=0;
-        Mesh(k).TabNsdF{1,km}(Pf) = km; 
+        %Pf = Mesh1(k).TabNsdF{1,km}~=0;
+        %Mesh(k).TabNsdF{1,km}(Pf) = km; 
     end 
+    Mesh(k) = NumFacet(utilMesh(Mesh(k)));
 end
+
+% Concatenate adjacent films
+%[Mesh,index] = OptimFreeForm(Mesh,index);
+% Simplify mesh
+%Mesh = SimplifyMesh(Mesh);
 end
